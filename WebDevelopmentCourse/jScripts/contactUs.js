4203d45372d84623262124cc704bedcaf18d61f8
WebDevelopmentCourse <WebDevelopmentCourseHTML5@gmail.com>
2017-04-01 14:49:08 +0300

    Update and rename contactScript1.js to contactUs.js

--- WebDevelopmentCourse/jScripts/contactUs.js
+++ WebDevelopmentCourse/jScripts/contactUs.js
@@ -32,17 +32,10 @@ function getFormValue() {
         var theValue = "";
 
         if ($(this).attr("for") != null) {
+
             theId = $(this).attr("for");
             theTitle = $(this).text();
-            // dealing with radiobutton
-            if ($("#" + theId).find("input").is(":visible")) {
-                $("#" + theId).find("input").each(function () {
-                    if ($(this).is(':checked')) {
-                        theValue += $("label[for='" + $(this).attr("id") + "']").text() + " + ";
-                    }
-                });
-            } else {  // if not radiobutton
-                theTitle = $(this).text();
+
                 if ($("textarea#" + theId).is(":visible")) {
                     theValue = $("textarea#" + theId).text();
                     theValue = $.trim(theValue);
@@ -52,24 +45,33 @@ function getFormValue() {
                 } else if ($("select#" + theId).is(":visible")) {
                     var optionValue = $("#" + theId).val();
                     theValue = $("#" + theId).find("option[value='" + optionValue + "']").text();
-                } else {
+
+                } else if ($("input#" + theId).is(":visible") && ($("input#" + theId).attr("type") == "text" || $("input#" + theId).attr("type") == "password")) {
                     theValue = $("#" + theId).val();
                 }
-            }
-
-        } else { // this is a checkbox
-            theId = $(this).attr("id");
-            theTitle = $("label[for='" + theId + "']").text();
-            if ($(this).is(':checked')) {
-                theValue = "כן";
-            } else {
-                theValue = "לא";
-            }
+                else {
+                    if ($("input#" + theId).is(':checked') && $("input#" + theId).attr("type") == "radio") {
+                            theValue += $("label[for='" + $(this).attr("id") + "']").text() + " + ";
+                        }
+                    else if ($("input#" + theId).attr("type") == "checkbox") {
+                        if ($("input#" + theId).is(':checked')) {
+                                theValue = "כן";
+                            }
+                            else {
+                                theValue = "לא";
+                            }
+                        }
+                   
+                }
+     
 
         }
 
-        theString += "<b>"+ theTitle + " : "+ "</b>" ;
-        theString += theValue + "<br/>";
+
+        if (theId != "") {
+            theString += "<b>" + theTitle + " : " + "</b>";
+            theString += theValue + "<br/>";
+        }
 
     });
     return theString;
@@ -78,40 +80,40 @@ function getFormValue() {
 
 function send2Server(str) {
     showAjaxLoader();
-    console.log("str: "+ str);
-    console.log("myEmail: "+ myEmail);
-     var theServer = "https://wwws.hit.ac.il/facebook/telemDev/TelemWebDevelopmentCourseContact_me.php";
-        $.ajax({
-                url: theServer,
-                type: "POST",
-                data: {
-                    theMailBody: str,
-                    sendTo: myEmail,
-                },
-                cache: false,
-                complete : function(data) {
-                console.log(data.statusText);
-                hideAjaxLoader();
-                $("div#feedback").html("");
-                addAjaxLoaderToFeedbackDiv();
-                if(data.statusText =="OK"){ 
-                        $("div#feedback").append(successFeedback);
-                        $("div#feedback").css("color", "green");
-                }
-                else{
-                       $("div#feedback").append(failFeedback);
-                       $("div#feedback").css("color", "red");
-                }
+    console.log("str: " + str);
+    console.log("myEmail: " + myEmail);
+    var theServer = "https://wwws.hit.ac.il/facebook/telemDev/TelemWebDevelopmentCourseContact_me.php";
+    $.ajax({
+        url: theServer,
+        type: "POST",
+        data: {
+            theMailBody: str,
+            sendTo: myEmail,
+        },
+        cache: false,
+        complete: function (data) {
+            console.log(data.statusText);
+            hideAjaxLoader();
+            $("div#feedback").html("");
+            addAjaxLoaderToFeedbackDiv();
+            if (data.statusText == "OK") {
+                $("div#feedback").append(successFeedback);
+                $("div#feedback").css("color", "green");
             }
-           
-  });
+            else {
+                $("div#feedback").append(failFeedback);
+                $("div#feedback").css("color", "red");
+            }
+        }
+
+    });
 }
 
 
 
 function myEncode(str) {
-  // return encodeURIComponent(str).replace(/\'/g, "%27");
-   // return encodeURIComponent(str).replace(/\'/g, "</br>");
+    // return encodeURIComponent(str).replace(/\'/g, "%27");
+    // return encodeURIComponent(str).replace(/\'/g, "</br>");
 }
 
 function addAjaxLoaderToFeedbackDiv() {
